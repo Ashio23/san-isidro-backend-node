1d5105dd413e597a40bcebf1a7aa60e34ffd7c90
Eduardo <eaacisternas@gmail.com>
2023-08-07 18:22:56 -0400

    feat(getDocument): adding functionality to proyect

--- src/infrastructure/modules/document/document.service.ts
+++ src/infrastructure/modules/document/document.service.ts
@@ -1,9 +1,14 @@
 import { IDocumentPort } from '@domain/Document';
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { DocumentResponseDto } from './entity';
+import {
+  SQL_ADAPTER,
+  SqlService,
+} from '@infrastructure/application/adapters/database';
 
 @Injectable()
 export class DocumentService implements IDocumentPort {
+  constructor(@Inject(SQL_ADAPTER) private readonly sqlService: SqlService) {}
   getDocuments(): Promise<DocumentResponseDto[]> {
     return Promise.resolve([
       { id: '1', description: 'Boleta Electrónica' },
@@ -19,5 +24,29 @@ export class DocumentService implements IDocumentPort {
       { id: '61', description: 'Electronic Credit Note' },
       { id: '801', description: 'Purchase Order' },
     ]);
+  }
+  async callDteMySql(
+    access: number,
+    processId: string,
+    routeId: string,
+    documentType: string,
+    branch: string,
+  ): Promise<unknown> {
+    const query = `
+    select
+      *
+    from
+      doc_cobro
+    where
+    id_proceso=${processId} and
+    id_servicio in (select id_servicio from servicios where id_ruta =(select id_ruta from rutas where encargado='${routeId}') and
+    ifnull(id_tipo_documento,1)= ${documentType} and
+    id_sucursal=(select id_sucursal from sucursal where nombre='${branch}'))`;
+
+    const result = await this.sqlService.query(query);
+    if (!result || Object.keys(result).length === 0) {
+      throw new Error(`No record found for processId: ${processId}`);
+    }
+    return result;
   }
 }
